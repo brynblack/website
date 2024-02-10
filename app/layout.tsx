278dd8c22a0dbf7404b1ac701ca6b1f6080ea55b
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2024-02-10 11:52:05 +1100

    fix: cleaned up code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Rubik } from "next/font/google";
 import "./globals.css";
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import Link from 'next/link'
 
-const inter = Inter({ subsets: ["latin"] });
+const rubik = Rubik({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Brynley's Website!",
@@ -26,7 +27,27 @@ export default function RootLayout({
         />
         <SpeedInsights/>
       </head>
-      <body className={inter.className}>{}</body>
+      <body className={rubik.className}>
+        <NavBar/>
+        {children}
+      </body>
     </html>
+  );
+}
+
+function NavBar() {
+  return (
+    <nav className="border-b border-neutral-800">
+      <div className="flex justify-between items-center w-full p-8">
+        <Link href="/">
+          <img src="/trans_flag.svg" alt="Transgender Flag" className="w-8 rounded"/>
+        </Link>
+        <div className="flex gap-10">
+          <Link href="/about">About</Link>
+          <Link href="/projects">Projects</Link>
+          <Link href="/contact">Contact</Link>
+        </div>
+      </div>
+    </nav>
   );
 }
