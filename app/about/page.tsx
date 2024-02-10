68cc30a723625789f4d0722ba9a34535cc572842
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-02-10 12:23:32 +1100

    fix: cleaned up /about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,12 +1,14 @@
 export default function Page() {
   return (
-    <main>
-      <h1>My name is Brynley Llewellyn-Roux.</h1>
-      <h2>I am a software engineer based in Sydney, Australia.</h2>
-      <p>I have 6 years of programming experience and I&apos;m currently studying at the University of Technology Sydney.</p>
-      <h3>I am open to work.</h3>
-      <img src="https://avatars.githubusercontent.com/u/49110391?v=4" alt="Profile Picture" />
-      <h2>You can check out more info about me by going to my GitHub <a href="https://github.com/brynblack"></a></h2>
+    <main className="flex gap-24 m-4">
+      <div className="flex flex-col gap-1">
+        <h1>My name is Brynley Llewellyn-Roux.</h1>
+        <h2>I am a software engineer based in Sydney, Australia.</h2>
+        <p>I have 6 years of programming experience and I&apos;m currently studying at the University of Technology Sydney.</p>
+        <h3>I am open to work.</h3>
+        <h2>You can check out more info about me by going to my GitHub <a href="https://github.com/brynblack"></a></h2>
+      </div>
+      <img src="https://avatars.githubusercontent.com/u/49110391?v=4" alt="Profile Picture" className="w-48 rounded-full"/>
     </main>
   );
 }
