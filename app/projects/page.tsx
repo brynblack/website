556d29a72dc8a50244b71e130c739ac8337ceb2d
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2024-02-10 23:12:34 +1100

    feat: redid about page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,16 +1,1 @@
-export default function Page() {
-  return (
-    <main className="flex justify-center">
-      <div className="flex flex-col flex-wrap gap-8 m-8 max-w-5xl w-full">
-        <div className="flex p-8 border border-neutral-800 rounded-lg">
-          <div>
-          </div>
-        </div>
-        <div className="flex p-8 border border-neutral-800 rounded-lg">
-          <div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
-}
+export default function Page() {}
--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-        port: '',
-        pathname: '/u/49110391',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
