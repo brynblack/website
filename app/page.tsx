da27a6637029cd58a7a28d8a4a9b94d42edc43b3
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2024-02-10 12:53:53 +1100

    fix: optimised images

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,26 @@
+import Image from 'next/image';
+
 export default function Page() {
   return (
     <main>
       <div className="flex flex-col gap-2 absolute bottom-0 m-12">
         <h1 className="text-4xl font-semibold">Welcome to my website!</h1>
         <h3 className="text-xl">Made with love by Brynley 💜</h3>
-        <div className="flex gap-4 items-center *:w-6 *:grayscale">
+        <div className="flex gap-4 items-center *:grayscale">
           <a href="https://github.com/brynblack" aria-label="GitHub">
-            <img src="/github.svg" alt="GitHub Logo"/>
+            <Image src="/github.svg" alt="GitHub Logo" width={24} height={24}/>
           </a>
           <a href="https://www.youtube.com/@brynblack9433" aria-label="YouTube">
-            <img src="/youtube.svg" alt="YouTube Logo"/>
+            <Image src="/youtube.svg" alt="YouTube Logo" width={24} height={24}/>
           </a>
           <a href="https://tech.lgbt/@brynblack" aria-label="Mastodon">
-            <img src="/mastodon.svg" alt="Mastodon Logo"/>
+            <Image src="/mastodon.svg" alt="Mastodon Logo" width={24} height={24}/>
           </a>
           <a href="https://twitch.tv/brynblack" aria-label="Twitch">
-            <img src="/twitch.svg" alt="Twitch Logo"/>
+            <Image src="/twitch.svg" alt="Twitch Logo" width={24} height={24}/>
           </a>
           <a href="https://steamcommunity.com/id/brynblack" aria-label="Steam">
-            <img src="/steam.svg" alt="Steam Logo"/>
+            <Image src="/steam.svg" alt="Steam Logo" width={24} height={24}/>
           </a>
         </div>
       </div>
