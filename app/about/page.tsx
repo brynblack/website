fe6a1eb483dcf8055a4f61e5f20cba4f0d8e63fe
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-02-11 19:28:18 +1100

    fix: made website more responsive

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -10,18 +10,17 @@ export const metadata: Metadata = {
 const About = () => {
   return (
     <Content>
-      <Title>About Me</Title>
       <ContentImage>
         <TextColumn>
-          <p>My name is Brynley Llewellyn-Roux.</p>
+          <p className="!text-3xl font-medium">My name is Brynley Llewellyn-Roux.</p>
           <p>I am a software engineer based in Sydney, Australia.</p>
           <p>I have 6 years of programming experience and I&apos;m currently working at Matrix AI.</p>
           <p>You can check out more info about me by going to my GitHub <a href="https://github.com/brynblack" className="text-pink-400">here</a>.</p>
         </TextColumn>
-        <Image src="/misc/pfp.png" alt="Profile Picture" width={192} height={192} className="rounded-full h-[192px] max-sm:hidden shadow-xl" />
+        <Image src="/misc/pfp.png" alt="Profile Picture" width={192} height={192} className="rounded-full h-[192px] shadow-xl" />
       </ContentImage>
       <ContentImage>
-        <Image src="/misc/icons.svg" width={554} height={161} className="max-lg:hidden" alt="Grid of Programming Languages"/>
+        <Image src="/misc/icons.svg" width={554} height={161} className="" alt="Grid of Programming Languages"/>
         <TextColumn>
           <p>These are just some of my skills.</p>
           <p>I have a very diverse skillset, ranging from web-based applications to low level embedded systems!</p>
@@ -34,7 +33,7 @@ const About = () => {
           <p>I believe all software should be free and open source, so my commitment to this is to keep all of my projects such as this website open source.</p>
           <p>Speaking of, if you would like to see the source code for this website, head to <a href="https://github.com/brynblack/website" className="text-pink-400">this link</a>.</p>
         </TextColumn>
-        <Image src="/socials/github.svg" alt="GitHub Logo" width={192} height={192} className="rounded-full h-[192px] max-sm:hidden dark:invert" />
+        <Image src="/socials/github.svg" alt="GitHub Logo" width={192} height={192} className="rounded-full h-[192px] dark:invert" />
       </ContentImage>
     </Content>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -3,10 +3,10 @@ import Image from 'next/image';
 const Home = () => {
   return (
     <main className="flex h-full">
-      <div className="flex flex-col gap-2 absolute bottom-0 m-12 slideright">
-        <h1 className="text-4xl font-medium">Welcome to my website!</h1>
-        <h3 className="text-xl">Made with love by Brynley 💜</h3>
-        <div className="flex gap-4 items-center *:grayscale hover:*:grayscale-0">
+      <div className="flex flex-col gap-2 absolute bottom-0 m-12 max-sm:m-8 max-sm:ml-4 slideright">
+        <h1 className="text-4xl max-sm:text-2xl font-medium">Welcome to my website!</h1>
+        <h3 className="text-xl max-sm:text-lg">Made with love by Brynley 💜</h3>
+        <div className="flex gap-4 items-center dark:invert hover:dark:*:invert *:grayscale hover:*:grayscale-0 *:transition-all duration-50">
           <a href="https://github.com/brynblack" aria-label="GitHub">
             <Image src="/socials/github.svg" alt="GitHub Logo" width={24} height={24}/>
           </a>
