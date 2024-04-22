ca61d4185f14b086e5a81cef8525e2eb7487ad00
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-04-22 23:28:06 +1000

    feat: added "blog"

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,10 +1,28 @@
 import type { Metadata } from "next";
+import { createClient } from "@supabase/supabase-js";
+import { Content } from "@/components/components";
+
+require('dotenv').config();
 
 export const metadata: Metadata = {
   title: "Brynley's Website! | Blog",
   description: "Keep in touch with what's happening.",
 };
 
-const Blog = () => {};
+async function Notes() {
+  const supabase = createClient(process.env.SUPABASE_URL ?? "", process.env.SUPABASE_ANON_KEY ?? "");
+
+  const { data: posts } = await supabase.from("posts").select();
+
+  return <pre>{JSON.stringify(posts, null, 2)}</pre>
+}
+
+const Blog = () => {
+  return (
+    <Content>
+      <Notes/>
+    </Content>
+  )
+};
 
 export default Blog;
