c046d26a76b97d6f2bd43ac2051eda314ce93b55
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2024-04-23 22:18:49 +1000

    fix: fixed metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,16 +1,30 @@
 import { Content } from "@/components/components";
 import { createClient } from "@/utils/supabase/server";
+import { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import React from "react";
 
+type Props = {
+  params: { slug: string };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const supabase = createClient();
+  const { data } = await supabase.from("posts").select().eq("id", params.slug);
+  const post = data?.at(0);
+  if (!post) return notFound();
+
+  return {
+    title: `Brynley's Website! | Blog | ${post.title}`,
+    description: post.description,
+  };
+}
+
 const Page = async ({ params }: { params: { slug: string } }) => {
   const supabase = createClient();
-
   const { data } = await supabase.from("posts").select().eq("id", params.slug);
-
   const post = data?.at(0);
-
   if (!post) return notFound();
 
   return (
@@ -22,7 +36,7 @@ const Page = async ({ params }: { params: { slug: string } }) => {
         <div className="mt-8 max-w-screen-md w-full">
           {post.contents.split(/\r\n|\n|\r/gm).map((line: string) => {
             return (
-              <React.Fragment>
+              <React.Fragment key={line}>
                 {line}
                 <br />
               </React.Fragment>
