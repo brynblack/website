68ab27aed5085676c73156da4f65631e14dcd5d7
Brynley Llewellyn-Roux <git.escalator677@passmail.com>
2024-07-13 18:46:20 +1000

    fix: improve responsiveness

--- app.vue
+++ app.vue
@@ -1,7 +1,25 @@
 <template>
   <Navbar />
+  <NuxtLoadingIndicator />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.325s;
+}
+.page-enter-from {
+  opacity: 0;
+  transform: translateX(-40px);
+  transition-timing-function: ease-out;
+}
+.page-leave-to {
+  opacity: 0;
+  transform: translateX(40px);
+  transition-timing-function: ease-in;
+}
+</style>
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  css: ['@/assets/css/main.scss']
+  css: ['@/assets/css/main.scss'],
+  app: {
+    pageTransition: { name: 'page', mode: 'out-in' }
+  },
 })
