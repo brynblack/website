8a8ad40678637395d72ae8aedcc5029ef842a7ba
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-07-14 16:37:00 +1000

    fix: vercel caching

--- pages/blog.vue
+++ pages/blog.vue
@@ -14,7 +14,10 @@ useSeoMeta({
         <span>This blog is a <Pink>heavy WIP</Pink>.</span>
         <span>Here, have this catgirl instead. :3</span>
       </div>
-      <img src="public/img/catgirl.webp" class="catgirl" />
+      <img
+        src="https://external-preview.redd.it/ASEE-uEwGHHstWWW2BH2vfJxw-clrRtetGyPrznOY44.png?auto=webp&s=395d2523b037e3e1a04d04d0ac13248f5c93c62e"
+        class="catgirl"
+      />
     </Container>
   </main>
 </template>
