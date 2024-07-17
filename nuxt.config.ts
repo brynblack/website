7908e922f4e979c6e2c7dfba7cdb351893dee691
Brynley Llewellyn-Roux <blynblack@gmail.com>
2024-07-17 11:40:42 +1000

    fix: security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,34 @@ export default defineNuxtConfig({
     headers: {
       crossOriginEmbedderPolicy:
         process.env.NODE_ENV === "development" ? "unsafe-none" : "require-corp",
+      contentSecurityPolicy:
+        process.env.NODE_ENV === "development"
+          ? false
+          : {
+              "script-src": [
+                "'self'",
+                "https:",
+                "'unsafe-inline'",
+                "'strict-dynamic'",
+                "'nonce-{{nonce}}'",
+              ],
+              "style-src": ["'self'", "https:", "'unsafe-inline'"],
+              "base-uri": ["'none'"],
+              "img-src": ["'self'", "data:"],
+              "font-src": ["'self'", "https:", "data:"],
+              "object-src": ["'none'"],
+              "script-src-attr": ["'none'"],
+              "upgrade-insecure-requests": true,
+            },
     },
+    corsHandler:
+      process.env.NODE_ENV === "development"
+        ? {
+            origin: "*",
+          }
+        : {
+            origin: "${serverUrl}",
+          },
   },
   modules: ["@nuxt/image", "nuxt-security"],
 });
