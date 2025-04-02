2b87dbb68763dac9687797fec22a2afa592baf6d
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-04-03 09:48:05 +1100

    fix: minor fixes

--- error.vue
+++ error.vue
@@ -1,14 +1,14 @@
 <script setup lang="ts">
 import type { NuxtError } from "#app";
 
-const props = defineProps({
+defineProps({
   error: Object as () => NuxtError,
 });
 </script>
 
 <template>
   <div>
-    <h1>{{ error.statusCode }}</h1>
+    <h1>{{ error!.statusCode }}</h1>
     <NuxtLink to="/">Go back home</NuxtLink>
   </div>
 </template>
