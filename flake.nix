1c96b5211c5905631e2cdd5c11e814d69b816cd5
Brynley Llewellyn-Roux <blynblack@gmail.com>
2025-04-03 09:35:02 +1100

    chore: updated flake and node to v23

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
         devShells.default = with pkgs;
           stdenv.mkDerivation {
             name = "env";
-            buildInputs = [ bun nodejs_20 ];
+            buildInputs = [ bun nodejs_23 ];
           };
       });
 }
